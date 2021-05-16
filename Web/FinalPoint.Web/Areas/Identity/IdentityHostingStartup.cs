696be8a1168fce8b266269bd7c42aa19cbdcf9a5
Nikolay Nikolaev <nikolainikolaev013@gmail.com>
2021-05-16 10:28:03 +0300

    Scaffold login + register pages using terminal and make them work with custom applicationuser

---
+++ Web/FinalPoint.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,24 @@
+using System;
+using FinalPoint.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FinalPoint.Web.Areas.Identity.IdentityHostingStartup))]
+namespace FinalPoint.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+            });
+        }
+    }
+}
