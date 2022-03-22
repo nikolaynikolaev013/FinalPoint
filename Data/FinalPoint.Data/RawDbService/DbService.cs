cefcab27100d3d100ed579e3095a78da25166e87
Nikolay.Nikolaev <nikolainikolaev013@gmail.com>
2022-03-22 12:55:17 +0200

    Add RawDbService to git

---
+++ Data/FinalPoint.Data/RawDbService/DbService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace FinalPoint.Data
+{
+    public class DbService : IDbService
+    {
+        private readonly IConfiguration configuration;
+
+        public DbService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public SqlConnection Conn { get; set; }
+
+        public SqlDataReader RunProcedure(SqlCommand command)
+        {
+            this.OpenConnection();
+            command.Connection = this.Conn;
+            SqlDataReader reader = command.ExecuteReader();
+            return reader;
+
+        }
+
+        private void OpenConnection()
+        {
+            var connetionString = this.configuration.GetConnectionString("DefaultConnection");
+            this.Conn = new SqlConnection(connetionString);
+            this.Conn.Open();
+        }
+    }
+}
