bf3aff1baeb3aefbc3e15f744e136b734200802f
Nikolay.Nikolaev <nikolainikolaev013@gmail.com>
2022-03-22 12:55:17 +0200

    Add RawDbService to git

---
+++ Data/FinalPoint.Data/RawDbService/IDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace FinalPoint.Data
+{
+    public interface IDbService
+    {
+        public SqlDataReader RunProcedure(SqlCommand command);
+
+    }
+}
